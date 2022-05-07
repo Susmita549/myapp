a93da403590ce82924ca8610d39b6d9ddc05baa2
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-05-07 16:08:07 +0530

    done

--- src/MyPractise/First/MyApp1.jsx
+++ src/MyPractise/First/MyApp1.jsx
@@ -9,7 +9,7 @@ export const MyApp1 = () => {
 
       <Navbar title="Textutils" about="ABOUT"/>
       <div className="container  my-3" >
-      <TextForm title="Enter Your Text Here..."/>
+      <TextForm title="Enter Your Text Heree..."/>
       </div>
 
     </>
--- src/MyPractise/First/TextForm.jsx
+++ src/MyPractise/First/TextForm.jsx
@@ -5,12 +5,12 @@ export default function TextForm(props) {
   return (
     <div>
 
-<h4>{props.title}</h4>
-<div className="mb-3">
-  
-  
-  <textarea className="form-control" id="myBox" rows="10"></textarea>
-</div>
+        <h4>{props.title}</h4>
+        <div className="mb-3">
+          <textarea className="form-control" id="myBox" rows="10"></textarea>
+        </div>
+        <button className="btn btn-primary">Convert to uppercase</button>
+
 
     </div>
   )
