6cd92e27560972385c1b539832342bb8d83128cc
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-05-07 16:38:50 +0530

    done

--- src/MyPractise/First/TextForm.jsx
+++ src/MyPractise/First/TextForm.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 // import './first.css'
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter Your Text...");
+  const handleChange=()=>{
+
+    let newText=text.toUpperCase()
+    setText(newText)
+  }
+
+  const handleChange1=()=>{
+    setText("Enter Your Text...")
+  }
+
+  const handleChange2=()=>{
+
+    let newText2=text.toLowerCase()
+    setText(newText2)
+  }
+  // const handleOnChange=(e)=>{
+  //   console.log("clicked..")
+  //   setText(e.target.value)
+  // }
   return (
     <div>
-
-        <h4>{props.title}</h4>
-        <div className="mb-3">
-          <textarea className="form-control" id="myBox" rows="10"></textarea>
-        </div>
-        <button className="btn btn-primary">Convert to uppercase</button>
-
-
+      <h4>{props.title}</h4>
+      <div className="mb-3">
+        <textarea className="form-control" value = {text} id="myBox" rows="10" onChange={(e) => setText(e.target.value)}></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleChange}>Convert to uppercase</button>
+      <button className="btn btn-primary mx-3" onClick={handleChange1}>Reset</button>
+      <button className="btn btn-primary mx-1" onClick={handleChange2}>Convert to lowercase</button>
     </div>
-  )
+  );
 }
