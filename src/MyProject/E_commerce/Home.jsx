86fc46aee423b92022445d57c6f2768038bd3c27
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-05-09 10:46:01 +0530

    done

--- src/MyProject/E_commerce/Home.jsx
+++ src/MyProject/E_commerce/Home.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStgIB9J4Vijz-QYQluGoHKHs9eH6ouf_ocNQ&usqp=CAU" class="d-block w-100" alt="IPHONE" height="500px"/>
+    </div>
+    <div class="carousel-item active">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCaeeCOJwxa_KSWf45uxWbx4o1SJuw8oZQpA&usqp=CAU" class="d-block w-100" alt="IPHONE" height="500px"/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSH8ZQqjtDQuUw_hdh-Q0PiM-v0R-P6Rb8Ag&usqp=CAU" class="d-block w-100" alt="IPHONE" height="500px"/>
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+    </>
   )
 }
 
