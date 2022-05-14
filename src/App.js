d9c2f8f9caffe40c8a3bfb5098e06f527537ffa1
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-05-14 19:41:10 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import MyCard from "./CreditCard/MyCard";
+import RestauentDetails from './Restaurant/RestaurentDetails';
 
 function App() {
   return (
     <div className="App">
      {/* <Main/> */}
-     {/* <RestauentDetails/> */}
-     <MyCard/>
+     <RestauentDetails/>
+     {/* <MyCard/> */}
     </div>
   );
 }
---
+++ src/App/Appp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appp = () => {
+  return (
+    <div>
+      hrllopp
+    </div>
+  );
+}
+
+export default Appp;
