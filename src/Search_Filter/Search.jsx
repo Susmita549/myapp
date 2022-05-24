a9bcf8044ba412d8f02841f38e673deffc44efcf
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-05-24 23:15:54 +0530

    completed

--- src/Search_Filter/Search.jsx
+++ src/Search_Filter/Search.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 const Search = () => {
+    const[filter,setFilter]=useState("")
+
+    const textChanged = (e) => {
+        setFilter(e.target.value)
+    }
+    
+    const dataSearch = data.cardData.filter((item)=>{
+        return Object.keys(item).some(key=>item[key].toString().toLowerCase()
+        .includes(filter.toString().toLowerCase()))
+    })
   return (
     <div>
       <section className="py-4 container border border-success">
         <div className="row justify-content-center">
-          {data.cardData.map((item, index) => {
+
+           <div className="col-12 mb-5">
+               <div className="mb-3 col-4 mx-auto text-center">
+                   <h1>Search</h1>
+                   <input type="text" 
+                   value={filter}
+                   onChange={textChanged.bind(this)}
+                   className="form-control"/>
+               </div>
+           </div>
+
+
+          {dataSearch.map((item, index) => {
             return (
               <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4" key={index}>
                 <div className=" card p-0 overflow-hidden h-100 shadow">
